9033d9b6ea892391d826e5142b3b1b8bc793e554
Telbit <t.t.bende@gmail.com>
2021-03-27 15:53:30 +0100

    Add PostController class

---
+++ Controllers/PostController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MyBlog.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyBlog.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PostController : ControllerBase
+    {
+        [HttpPost("edit")]
+        public async Task Edit(Post post)
+        {
+            Console.WriteLine($"{post.Title} {post.Body}");
+        }
+    }
+}
