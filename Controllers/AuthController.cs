9cbc2d5bf7abdfea2041826f0f2725eea0feeeed
Telbit <t.t.bende@gmail.com>
2021-04-06 17:18:38 +0200

    Add AuthController with login

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MyBlog.Services;
+using MyBlog.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyBlog.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IUserService userService;
+
+        public AuthController(IUserService userService)
+        {
+            this.userService = userService;
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> LoginAsync(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await userService.LoginUserAsync(model);
+
+                if (result.IsSuccess)
+                {
+                    return Ok(result);
+                }
+                return BadRequest(result);
+            }
+
+            return BadRequest("Some properties are not valid");
+        }
+    }
+}
