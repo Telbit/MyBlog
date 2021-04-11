27e0bd27f9aba033be83a3a47b4ab2b0fb29d5b6
Telbit <t.t.bende@gmail.com>
2021-04-11 17:55:43 +0200

    Add login route

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Route, Switch } from "react-router";
+import { Login } from "./components/Auth/Login";
 import { Edit } from "./components/home/Edit";
 import { Home } from "./components/home/Home";
 import { Post } from "./components/home/Post";
@@ -14,6 +15,7 @@ export const App = () => {
                 <Route path={"/post/:id"} component={Post}></Route>
                 <Route path={"/create"} exact component={Edit}></Route>
                 <Route path={"/edit/:id"} component={Edit}></Route>
+                <Route path={"/login"} component={Login}></Route>
             </Switch>
         </div>
     );
